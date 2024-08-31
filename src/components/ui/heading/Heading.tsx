0081803e2acc7627d4ea077bb4c0b5535ef7b5cd
ppppppsmash <pppppp.work@gmail.com>
2024-08-31 14:08:23 +0900

    feat: add align prop to Heading components and change directory

--- src/components/ui/heading/Heading.tsx
+++ src/components/ui/heading/Heading.tsx
@@ -3,14 +3,17 @@ import classes from './Heading.module.css';
 interface HeadingProps {
   level: 1 | 2 | 3 | 4 | 5 | 6;
   children: React.ReactNode;
+  align?: 'center' | 'left' | 'right';
 }
 
-export const Heading = ({ level, children }: HeadingProps) => {
+export const Heading = ({ level, children, align = 'left' }: HeadingProps) => {
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
 
   return (
     <div className={classes.headingWrapper}>
-      <Tag className={classes.heading}>{children}</Tag>
+      <Tag className={classes.heading} style={{ textAlign: align }}>
+        {children}
+      </Tag>
     </div>
   );
 };
