51740f32fd99cf85db4b58f65b641e7c9a06eeaa
ppppppsmash <pppppp.work@gmail.com>
2024-08-31 22:01:39 +0900

    refactor: memoize getPopulation with useCallback to enhance render

--- src/hooks/usePrefecturePopulation.ts
+++ src/hooks/usePrefecturePopulation.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 import { fetchPrefectures, fetchPopulation } from '../api/fetchUtils';
 import { Prefecture, PrefectureList, PopulationType } from '../types';
@@ -20,26 +20,28 @@ export const usePrefecturePopulationHooks = () => {
     setSelectedPrefectures([...selectedPrefectures, prefecture]);
   };
 
-  const getPopulation = async (prefName: string, prefCode: number) => {
-    const data = await fetchPopulation(prefCode);
-    console.log(data);
-    setPopulation((prev) => {
-      if (!prev.some((p) => p.prefecture === prefName)) {
-        const types = data.map((t: PopulationType) => t.label);
-        setPopulationTypes((prevTypes) =>
-          Array.from(new Set([...prevTypes, ...types])),
-        );
-      }
+  const getPopulation = useCallback(
+    async (prefName: string, prefCode: number) => {
+      const data = await fetchPopulation(prefCode);
+      setPopulation((prev) => {
+        if (!prev.some((p) => p.prefecture === prefName)) {
+          const types = data.map((t: PopulationType) => t.label);
+          setPopulationTypes((prevTypes) =>
+            Array.from(new Set([...prevTypes, ...types])),
+          );
+        }
 
-      return [
-        ...prev,
-        {
-          prefecture: prefName,
-          population: data,
-        },
-      ];
-    });
-  };
+        return [
+          ...prev,
+          {
+            prefecture: prefName,
+            population: data,
+          },
+        ];
+      });
+    },
+    [],
+  );
 
   useEffect(() => {
     fetchPrefectures().then((data) => {
@@ -48,10 +50,32 @@ export const usePrefecturePopulationHooks = () => {
   }, []);
 
   useEffect(() => {
+    const fetchInitialData = async () => {
+      const prefecturesData = await fetchPrefectures();
+      setPrefectures(prefecturesData);
+
+      const initialPopulation = await fetchPopulation(1);
+      const types = initialPopulation.map((item: PopulationType) => item.label);
+      setPopulationTypes(types);
+      setSelectedType(types[0]);
+    };
+
+    fetchInitialData();
+  }, []);
+
+  useEffect(() => {
+    selectedPrefectures.forEach((prefecture) => {
+      if (!population.some((p) => p.prefecture === prefecture.prefName)) {
+        getPopulation(prefecture.prefName, prefecture.prefCode);
+      }
+    });
+  }, [selectedPrefectures, population, getPopulation]);
+
+  useEffect(() => {
     selectedPrefectures.forEach((prefecture) => {
       getPopulation(prefecture.prefName, prefecture.prefCode);
     });
-  }, [selectedPrefectures]);
+  }, [getPopulation, selectedPrefectures]);
 
   return {
     prefectures,
